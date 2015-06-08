9f4e9bb82630b1ebb64cf4c0eae92f71b875ba68
wenjun.yan <mylastnameisyan@gmail.com>
2015-06-09 02:47:10 +0900

    worker.Name(); Graceful shutdown;

--- worker.go
+++ worker.go
@@ -2,7 +2,14 @@ package main
 
 import (
         "encoding/json"
+        "fmt"
         "log"
+        "math/rand"
+        "os"
+        "os/signal"
+        "strings"
+        "sync"
+        "syscall"
 )
 
 // Exmaples:
@@ -38,6 +45,7 @@ func (f HandleFunc) ServeJob(j *Job) error { return f(j) }
 type Worker struct {
         rconn    Conn
         wconn    Conn
+        once     sync.Once
         done     chan bool
         handlers map[string]Handler
 }
@@ -55,6 +63,33 @@ var (
         ListenAndServe = worker.ListenAndServe
 )
 
+func (w *Worker) handleSignals() {
+        sigs := make(chan os.Signal, 1)
+        signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+        go func() {
+                s := <-sigs
+                log.Println("Signal received:", s)
+                w.Done()
+        }()
+}
+
+func (w *Worker) Name() (name string, err error) {
+        queues := []string(nil)
+        for queue, _ := range w.handlers {
+                queues = append(queues, queue[6:])
+        }
+        qname := strings.Join(queues, ",")
+
+        var host string
+        host, err = os.Hostname()
+        if err != nil {
+                return
+        }
+        pid := os.Getpid()
+        name = fmt.Sprintf("%s:%d:%s:%d", host, pid, qname, rand.Int())
+        return
+}
+
 func (w *Worker) ListenAndServe(url string) error {
         var err error
         w.rconn, err = NewConn(url)
@@ -65,12 +100,13 @@ func (w *Worker) ListenAndServe(url string) error {
         if err != nil {
                 return err
         }
+        w.handleSignals()
         w.loop()
         return nil
 }
 
 func (w *Worker) Handle(queue string, h Handler) { w.handlers[JobQueueName(queue)] = h }
-func (w *Worker) Done()                          { close(w.done) }
+func (w *Worker) Done()                          { w.once.Do(func() { close(w.done) }) }
 
 func (w *Worker) accept() (jobs chan *Job) {
         jobs = make(chan *Job)
